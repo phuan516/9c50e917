e6a4cc42e65b95c864033de8ac5efede4a964887
phuan516 <phuan516@mtroyal.ca>
2024-12-21 18:28:01 -0700

    basic bottom nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useState } from "react";
+import { FaArchive, FaStar, FaRegClock } from "react-icons/fa";
+import { MdOutlineContacts } from "react-icons/md";
 
-import Header from "./Header.jsx";
+import Header from "./components/Header.jsx";
 import useActivities from "./hooks/useActivities.js";
+import "./css/nav.css";
 
 const App = () => {
+  const [currentTab, setCurrentTab] = useState("recent");
+
   const { active, archive, callDetails, fetchCallDetails, setIsArchived } =
     useActivities();
 
@@ -11,68 +17,117 @@ const App = () => {
     <div className="container">
       <Header />
       <div className="container-view">
-        <button
+        {currentTab === "recent" && (
+          <>
+            <button
+              onClick={() => {
+                active.forEach((activity) => {
+                  setIsArchived(activity.id, true);
+                });
+              }}
+            >
+              archive all
+            </button>
+            <h2>Recent</h2>
+            <ul>
+              {active.map((activity, index) => (
+                <li key={index}>
+                  <button
+                    onClick={() => {
+                      fetchCallDetails(activity.id);
+                    }}
+                  >
+                    {activity.id}
+                  </button>
+                  <button
+                    onClick={() => {
+                      setIsArchived(activity.id, !activity.is_archived);
+                    }}
+                  >
+                    Archive
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+        {currentTab === "archive" && (
+          <>
+            <button
+              onClick={() => {
+                archive.forEach((activity) => {
+                  setIsArchived(activity.id, false);
+                });
+              }}
+            >
+              unarchive all
+            </button>
+            <h2>Archived</h2>
+            <ul>
+              {archive.map((activity, index) => (
+                <li key={index}>
+                  <button
+                    onClick={() => {
+                      fetchCallDetails(activity.id);
+                    }}
+                  >
+                    {activity.id}
+                  </button>
+                  <button
+                    onClick={() => {
+                      setIsArchived(activity.id, !activity.is_archived);
+                    }}
+                  >
+                    Unarchived
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+
+        {currentTab === "details" && (
+          <>
+            <h2>Details</h2>
+            {callDetails && <div>{JSON.stringify(callDetails)}</div>}
+          </>
+        )}
+      </div>
+      <div className="bottom-nav">
+        <div className="nav-item">
+          <span className="nav-icon">
+            <FaStar />
+          </span>
+          <span className="nav-label">Favorites</span>
+        </div>
+        <div
+          className="nav-item"
           onClick={() => {
-            active.forEach((activity) => {
-              setIsArchived(activity.id, true);
-            });
+            setCurrentTab("Recent");
           }}
         >
-          archive all
-        </button>
-        <button
+          <span className="nav-icon">
+            <FaRegClock />
+          </span>
+          <span className="nav-label">Recent</span>
+        </div>
+        <div
+          className="nav-item"
           onClick={() => {
-            archive.forEach((activity) => {
-              setIsArchived(activity.id, false);
-            });
+            setCurrentTab("Recent");
           }}
         >
-          unarchive all
-        </button>
-        <h2>Active</h2>
-        <ul>
-          {active.map((activity, index) => (
-            <li key={index}>
-              <button
-                onClick={() => {
-                  fetchCallDetails(activity.id);
-                }}
-              >
-                {activity.id}
-              </button>
-              <button
-                onClick={() => {
-                  setIsArchived(activity.id, !activity.is_archived);
-                }}
-              >
-                Archive
-              </button>
-            </li>
-          ))}
-        </ul>
-        <h2>Archived</h2>
-        <ul>
-          {archive.map((activity, index) => (
-            <li key={index}>
-              <button
-                onClick={() => {
-                  fetchCallDetails(activity.id);
-                }}
-              >
-                {activity.id}
-              </button>
-              <button
-                onClick={() => {
-                  setIsArchived(activity.id, !activity.is_archived);
-                }}
-              >
-                Unarchived
-              </button>
-            </li>
-          ))}
-        </ul>
-        <h2>Details</h2>
-        {callDetails && <div>{JSON.stringify(callDetails)}</div>}
+          <span className="nav-icon">
+            <FaArchive />
+          </span>
+          <span className="nav-label">Archive</span>
+        </div>
+        <div className="nav-item">
+          <span className="nav-icon">
+            <MdOutlineContacts />
+          </span>
+          <span className="nav-label">Contact</span>
+        </div>
       </div>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { FaSearch, FaBars } from "react-icons/fa";
 
-import "./css/header.css";
+import "../css/header.css";
 
 const Header = () => {
   return (
     <header>
       <h1>aircall</h1>
       <div className="icons">
-        <FaSearch size={20} className="search" />
-        <FaBars size={20} className="menu" />
+        <div className="search-icon">
+          <FaSearch size={20} className="search" />
+        </div>
+        <div className="menu-icon">
+          <FaBars size={20} className="menu" />
+        </div>
       </div>
     </header>
   );
